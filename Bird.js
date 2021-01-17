d8a652c05fbc49f3647e92c5a6db1209d65566b5
Peeyush0106 <peeyush.agarwal0106@gmail.com>
2021-01-17 15:54:52 +0530

    Class 31. Added Day and Night Time Effect. Learnt JSON and API calls

---
+++ Bird.js
@@ -0,0 +1,36 @@
+class Bird {
+  constructor(x, y) {
+    var options = {
+      'restitution': 0.8,
+      'friction': 1.0,
+      'density': 1.0
+    }
+    this.radius = 25;
+    this.diameter = this.radius * 2;
+    this.body = Bodies.circle(x, y, this.radius, options);
+    World.add(world, this.body);
+
+    this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
+  }
+
+  display() {
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.diameter,this.diameter);
+    pop();
+
+    if (this.body.velocity.x > 10 && this.body.position.x > 200) {
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+
+    for (var i = 0; i < this.trajectory.length; i++) {
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+    }
+  }
+}
